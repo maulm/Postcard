8ac4d8300b861621d91a9246c30f8fd0a68993ce
maulm <mauricio.miotto@gmail.com>
2015-03-10 23:04:51 -0300

    added a comment to test additional commig

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //Adding a comment
+        //One other comment
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
